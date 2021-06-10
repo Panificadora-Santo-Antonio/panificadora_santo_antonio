a08aed63a1d279d642804a07fdbb4ae022a25058
Hermyson <hermysoncassiano@gmail.com>
2021-06-09 21:46:20 -0300

    Criação tabela estoque

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_230917) do
+ActiveRecord::Schema.define(version: 2021_06_09_130826) do
+
+  create_table "estoques", force: :cascade do |t|
+    t.integer "quantidadeProduto"
+    t.decimal "valorTotalEstoque"
+    t.integer "produto_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["produto_id"], name: "index_estoques_on_produto_id"
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
@@ -19,8 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_230917) do
     t.date "validade"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    
-ActiveRecord::Schema.define(version: 2021_06_08_230059) do
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_06_08_230059) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "estoques", "produtos"
 end

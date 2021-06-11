907bb0616a44e73f339d025b474a35e812a3ad9c
Hermyson <hermysoncassiano@gmail.com>
2021-06-10 22:13:04 -0300

    Corrigindo problemas apontados pelo Codacy

--- app/controllers/estoques_controller.rb
+++ app/controllers/estoques_controller.rb
@@ -60,7 +60,7 @@ class EstoquesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_estoque
-      @estoque = Estoque.find(params[:id])
+      @estoque = Estoque.where("id = ?", params[:id]).first
     end
 
     # Only allow a list of trusted parameters through.

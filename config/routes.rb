cab543ddf069a79ed5a47815b03f4fd19e8e4f15
laisy <https://github.com/laisy>
2021-06-10 22:12:02 -0300

    Colocando a rota de devise usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-
+  devise_for :users, :path => "usuarios", 
+  :path_names => { :sign_in => 'login', 
+            :sign_out => 'logout', 
+            :password => 'secret', 
+            :confirmation => 'verification', 
+            :unlock => 'unblock', 
+            :registration => 'register', 
+            :sign_up => 'cmon_let_me_in' }
   resources :produtos
-  devise_for :users
-  root 'home#index'
+  #root 'home#index'
 end

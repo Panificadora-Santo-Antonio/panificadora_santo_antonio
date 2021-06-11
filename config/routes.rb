c2deaeec4102f600023ee84d327b00c4a24bbf9b
laisy <https://github.com/laisy>
2021-06-10 23:59:33 -0300

    Ajustando home index com informações de login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => "usuarios", 
-  :path_names => { :sign_in => 'login', 
-            :sign_out => 'logout', 
-            :password => 'secret', 
-            :confirmation => 'verification', 
-            :unlock => 'unblock', 
-            :registration => 'register', 
-            :sign_up => 'cmon_let_me_in' }
+  devise_for :users
   resources :produtos
   root 'home#index'
 end

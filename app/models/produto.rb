2f6586d4c2403c4b1e17db9e0d3f03424389a90c
Allysson Guimarães <allysson.guimaraes042@gmail.com>
2021-06-13 17:13:05 -0300

    alteração na validação de produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,7 +2,7 @@ class Produto < ApplicationRecord
 
   validates :nome, presence: true, length:{maximum: 50}
   validates :descrição, length:{maximum: 200}
-  validates :preco, presence: true, :numericality => true
+  validates :preco, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
   validates_datetime :validade, after: :today 
   has_one :estoque, dependent: :destroy
   accepts_nested_attributes_for :estoque

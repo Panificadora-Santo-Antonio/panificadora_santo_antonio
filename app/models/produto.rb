69ed048e7c420a696e3f460ca3418131eb771887
Allysson Guimarães <allysson.guimaraes042@gmail.com>
2021-06-13 15:07:51 -0300

    validação de produto e modificação das views de produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,4 +1,8 @@
 class Produto < ApplicationRecord
+
+  validates :nome, presence: true, length:{maximum: 50}
+  validates :descrição, length:{maximum: 200}
+  validates :preco, presence: true, :numericality => true
   has_one :estoque, dependent: :destroy
   accepts_nested_attributes_for :estoque
 end

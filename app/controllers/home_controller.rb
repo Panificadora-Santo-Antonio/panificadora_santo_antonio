b57adee825a8773837967bcfd9f1b0136987de44
laisy <https://github.com/laisy>
2021-06-14 02:12:44 -0300

    Verificar e criar admin padrão do sistema

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
+  before_action :check_admin 
   def index
     @message = "Panificadora Santo Antonio!"
     end
+
+  def check_admin
+    if carry_admin.nil?
+      create_admin
+    end
+  end
+
+  def carry_admin
+    User.find_by(email: "admin@admin.com")
+  end
+
+  def create_admin
+    admin = User.new(email: "admin@admin.com", password: "123456", name: "admin", role: "Admin")
+    admin.save()
+  end
 end

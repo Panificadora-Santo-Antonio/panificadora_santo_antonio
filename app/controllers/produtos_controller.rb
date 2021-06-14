27d56d3368d7c48e4131e27141111f1e50b9eab5
laisy <https://github.com/laisy>
2021-06-13 23:56:09 -0300

    Adicionando o check role em produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,7 @@
 class ProdutosController < ApplicationController
   before_action :authenticate_user!
   before_action :set_produto, only: %i[ show edit update destroy ]
+  before_action :check_role, unless: %i[ show index ]
 
   # GET /produtos or /produtos.json
   def index

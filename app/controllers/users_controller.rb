2b5ff66c53995de0fb612d8f623fb9ed64bf56d4
laisy <https://github.com/laisy>
2021-06-14 21:28:46 -0300

    Corrigindo funcionalidade edit de user list para admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :check_role, only: %i[ index ]
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def index

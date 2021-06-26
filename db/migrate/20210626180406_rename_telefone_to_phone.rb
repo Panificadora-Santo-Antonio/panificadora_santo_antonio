7b3d5a502bd8e3692fde2c2f898bd47336fc855b
laisy <https://github.com/laisy>
2021-06-26 17:49:54 -0300

    Correção de erros apontados ao dá o rake db:migrate

--- db/migrate/20210626180406_rename_telefone_to_phone.rb
+++
@@ -1,5 +0,0 @@
-class RenameTelefoneToPhone < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :customers, :telefone, :phone
-  end
-end
--- db/migrate/20210626190833_create_sales.rb
+++ db/migrate/20210626190833_create_sales.rb
@@ -2,7 +2,7 @@ class CreateSales < ActiveRecord::Migration[6.0]
   def change
     create_table :sales do |t|
       t.decimal :totalValue
-      t.date_time :date_time
+      t.datetime :date_time
 
       t.timestamps
     end

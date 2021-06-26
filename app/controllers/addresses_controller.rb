ba8f32e148ba3f41925c2bd59190e6cc9c112083
laisy <https://github.com/laisy>
2021-06-25 22:27:16 -0300

    Corrigindo erro no set_address apontado pelo codacy

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -59,7 +59,7 @@ class AddressesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_address
-      @address = Address.find(params[:id])
+      @address = Address.where("id = ?", params[:id]).first
     end
 
     # Only allow a list of trusted parameters through.

a386175eb3d6699eaa745025a3b2169d7f67dd40
laisy <https://github.com/laisy>
2021-06-26 15:25:18 -0300

    Retirando rota de endereço desnecessárias

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :addresses
-  resources :customers
   root 'home#index'
   devise_for :users, :path_prefix => 'my',
     controllers: {:registrations => "registrations"}
   resources :produtos
   resources :estoques
+  resources :customers
   resources :users, :only => %i[show index]
   as :user do 
     get "/register", to: "registrations#new", as: "register"

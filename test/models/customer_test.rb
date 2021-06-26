3abc73fcf88591cee72dd40e0592032f190e3aae
laisy <https://github.com/laisy>
2021-06-26 19:37:43 -0300

    Testes de model de Cliente

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new customer without valid nome" do
+    customer = Customer.new(name:'', phone: 87996226421)
+    assert_not customer.save
+    end
+
+  test "new customer" do
+    customer = Customer.new(name:'Lucia', phone: 87996226421)
+    assert customer.save
+    end
+
+  test "update name customer" do
+    customer = Customer.new(name:'Lucia', phone: 87996226421)
+    assert customer.save
+    assert Customer.update(name:'Lucia Maria')
+    end
+  
 end

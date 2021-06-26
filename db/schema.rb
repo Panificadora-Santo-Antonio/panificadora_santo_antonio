60ed3d040c3da87acb5f6281d74912de3769340e
laisy <https://github.com/laisy>
2021-06-26 15:27:05 -0300

    Renomeando coluna telefone para phone em cliente

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_001859) do
+ActiveRecord::Schema.define(version: 2021_06_26_180406) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "zip_code"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_06_26_001859) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
-    t.string "telefone"
+    t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

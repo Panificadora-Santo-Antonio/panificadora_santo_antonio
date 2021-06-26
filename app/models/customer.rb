ab0b400db54029c544ff64fb403ab49fc37fdeb3
laisy <https://github.com/laisy>
2021-06-26 17:01:56 -0300

    Add validates de cliente

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_one :address
   accepts_nested_attributes_for :address, allow_destroy: true
+
+  validates :name, presence: true, presence: true, length: {minimum: 3, too_short: "%{count} characters is the minimum allowed ", maximum: 50, too_long: "%{count}characters is the maximum allowed " }
+  validates :phone, presence: true, numericality: { only_integer: true }, length: {is: 11}
 end

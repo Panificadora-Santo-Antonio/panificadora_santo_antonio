bcaff87fce734cacb53012dea48bb1280eccee45
Hermyson <hermysoncassiano@gmail.com>
2021-06-28 17:11:12 -0300

    Corringindo vunerabilidades apontada pelo codacy em search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,33 +10,23 @@ class SearchController < ApplicationController
     end
 
     if params[:start_date].present? && params[:final_date].present?
-      start_date = DateTime.parse(params[:start_date]).beginning_of_day
-      final_date = DateTime.parse(params[:final_date]).end_of_day
-      start = (params[:start_date])
-      final = (params[:final_date])
-      @sales = @sales.where("sales.created_at >= '" + start + "'")
-      @sales = @sales.where("sales.created_at <= '" + final + "'")
+      @sales = @sales.where(["sales.created_at >= ? AND sales.created_at <= ? " , "#{params[:start_date]}","#{params[:final_date]}"])
+
     elsif  params[:start_date].present? && params[:final_date].blank?
-      start = (params[:start_date])
-      start_date = DateTime.parse(params[:start_date]).beginning_of_day
-      @sales = @sales.where("sales.created_at >= '" + start + "'")
+      @sales = @sales.where(["sales.created_at >= ? ","#{params[:start_date]}"])
+
     elsif  params[:start_date].blank? && params[:final_date].present?
-      final = (params[:final_date])
-      final_date = DateTime.parse(params[:final_date]).end_of_day
-      @sales = @sales.where("sales.created_at <= '" + final + "'")
+      @sales = @sales.where(["sales.created_at <= ? ","#{params[:final_date]}"])
     end
 
     if params[:value_of].present? && params[:value_up_to].present?
-      valueOf = (params[:value_of])
-      valueUpTo = (params[:value_up_to])
-      @sales = @sales.where("sales.totalValue >= '" + valueOf + "'")
-      @sales = @sales.where("sales.totalValue <= '" + valueUpTo + "'")
+      @sales = @sales.where(["sales.totalValue >= ? AND sales.totalValue <= ? " , "#{params[:value_of]}","#{params[:value_up_to]}"])
+      #@sales = @sales.where("sales.totalValue >= '" + valueOf + "'")
+      #@sales = @sales.where("sales.totalValue <= '" + valueUpTo + "'")
     elsif params[:value_of].present? && params[:value_up_to].blank?
-      valueOf = (params[:value_of])
-      @sales = @sales.where("sales.totalValue >= '" + valueOf + "'")
+      @sales = @sales.where(["sales.totalValue >= ?","#{params[:value_of]}"])
     elsif  params[:value_of].blank? && params[:value_up_to].present?
-      valueUpTo = (params[:value_up_to])
-      @sales = @sales.where("sales.totalValue <= '" + valueUpTo + "'")
+      @sales = @sales.where(["sales.totalValue <= ?","#{params[:value_up_to]}"])
     end
 
     @sales_total = @sales

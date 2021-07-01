ba8c5054a79a9ad8dc0b86d55d4efbf3ba38ec81
laisy <https://github.com/laisy>
2021-07-01 20:29:10 -0300

    Inserindo os asserts no teste model user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,13 +8,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "editando user" do
     user = User.new(email: 'pedro@email.com', password: '123456', name:'Pedro', role: 'Admin')
-    user.save
+    assert user.save
     assert user.update(email: 'pedro@email.com', password: '123456', name:'Pedro Jorge', role: 'Admin')
     end
 
   test "editando user com nome com um caracte" do
     user = User.new(email: 'pedro@email.com', password: '123456', name:'Pedro', role: 'Admin')
-    user.save
+    assert user.save
     assert_not user.update(email: 'pedro@email.com', password: '123456', name:'P', role: 'Admin')
     end
 

ae4b35aaa2f81b5fe4c875a3486d8e19cd63a469
Allysson Guimarães <allysson.guimaraes042@gmail.com>
2021-07-01 21:55:15 -0300

    adicão do relacionamento entre customer e sale na model de customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,8 @@
 class Customer < ApplicationRecord
   has_one :address
   accepts_nested_attributes_for :address, allow_destroy: true
-
+  has_many :sales
+  accepts_nested_attributes_for :sales, allow_destroy: true
   validates :name, presence: true, length: {minimum: 3, too_short: "%{count} characters is the minimum allowed ", maximum: 50, too_long: "%{count}characters is the maximum allowed " }
   validates :phone, presence: true, numericality: { only_integer: true }, length: {is: 11}
 end

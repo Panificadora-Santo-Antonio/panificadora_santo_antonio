9f5f2e688673d8d4b6a72a8932e22e6d22e3748e
laisy <laisyferreira2011@gmail.com>
2021-07-03 00:00:29 -0300

    Adicionando a rota para user destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   
   devise_for :users, :path_prefix => 'my',
     controllers: {:registrations => "registrations"}
+  
   resources :produtos
   resources :customers
 
@@ -19,7 +20,7 @@ Rails.application.routes.draw do
   end
 
   
-  resources :users, :only => %i[show index]
+  resources :users, :only => %i[show index destroy]
   as :user do 
     get "/register", to: "registrations#new", as: "register"
   end

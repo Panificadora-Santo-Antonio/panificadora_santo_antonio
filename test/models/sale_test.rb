84e73d7e894e29ea7fed0cf6a40070bb159b2b77
laisy <laisyferreira2011@gmail.com>
2021-07-04 22:34:12 -0300

    Colocando user_id no teste que possui Sale

--- test/models/sale_test.rb
+++ test/models/sale_test.rb
@@ -11,16 +11,20 @@ class SaleTest < ActiveSupport::TestCase
   end
 
   test 'should save sale with totalValue and customer_id' do
+    user = User.new(email: 'pedro@email.com', password: '123456', name:'Pedro', role: 'Admin')
+    assert user.save
     customer = Customer.new(name:'Lucia', phone: 87996226421)
     assert customer.save
-    sale = Sale.new(customer_id:customer.id,totalValue:"0")
+    sale = Sale.new(totalValue: "0", customer_id: customer.id, user_id: user.id)
     assert sale.save
   end
 
   test 'should not save sale with totalValue < 0' do
+    user = User.new(email: 'pedro@email.com', password: '123456', name:'Pedro', role: 'Admin')
+    assert user.save
     customer = Customer.new(name:'Lucia', phone: 87996226421)
     assert customer.save
-    sale = Sale.new(customer_id:customer.id,totalValue:"-1")
+    sale = Sale.new(totalValue: "-1", customer_id: customer.id, user_id: user.id)
     assert_not sale.save
   end
 end

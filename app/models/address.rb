a769e307d4d28d4542b68856431dff89c7e0bed4
laisy <laisyferreira2011@gmail.com>
2021-07-04 23:15:37 -0300

    Atualizando idioma para português de mensagens de model

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,10 +2,10 @@ class Address < ApplicationRecord
   belongs_to :customer
 
   validates :zip_code, presence: true, numericality: { only_integer: true }, length: {is: 8}
-  validates :city, presence: true, length: {minimum: 3, too_short: "%{count} characters is the minimum allowed ", maximum:50, too_long: "%{count}characters is the maximum allowed " }
-  validates :district, presence: true, length: {minimum: 3, too_short: "%{count} characters is the minimum allowed ", maximum:30, too_long: "%{count}characters is the maximum allowed " }
-  validates :road, presence: true, length: {minimum: 3, too_short: "%{count} characters is the minimum allowed ", maximum:60, too_long: "%{count}characters is the maximum allowed " }
-  validates :number, presence: true,  numericality: { only_integer: true }, length: {minimum: 1, too_short: "%{count} characters is the minimum allowed "}
-  validates :complement, length: {maximum:50, too_long: "%{count}characters is the maximum allowed " }
+  validates :city, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o mínimo permitido", maximum:50, too_long: "%{count} caracteres é o máximo permitido" }
+  validates :district, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o mínimo permitido", maximum:30, too_long: "%{count} caracteres é o máximo permitido" }
+  validates :road, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o mínimo permitido", maximum:60, too_long: "%{count} caracteres é o máximo permitido" }
+  validates :number, presence: true,  numericality: { only_integer: true }, length: {minimum: 1, too_short: "%{count} caracteres é o mínimo permitido"}
+  validates :complement, length: {maximum:50, too_long: "%{count} caracteres é o mínimo permitido" }
 
 end
